8061cb044a675e6de8be63585b38dc21e39572fd
KwangHo <skh990427@naver.com>
2024-02-20 16:48:47 +0900

    JPQL 기본 문법 .getSingleResult, getResultList()

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,9 +1,8 @@
 package jpql;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,6 +17,16 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+
+            List<Member> resultList = query.getResultList(); //반환타입이 여러개일 때는 .getResultList()
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+            Query query3 = em.createQuery("select m.username, m.age from Member m"); //타입 정보를 받을 수 없을 때
+
 
             tx.commit();
         } catch (Exception e) {
