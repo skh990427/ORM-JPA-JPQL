e9c9720289be5c67a3e5a8b57dd936dd8a45db75
KwangHo <skh990427@naver.com>
2024-02-20 17:13:18 +0900

    프로젝션 - 엔티티 프로젝션(영속성 컨텍스트에서 관리됨)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -18,17 +18,13 @@ public class JpaMain {
             member.setUsername("member1");
             em.persist(member);
 
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
+            em.flush();
+            em.clear();
 
-            //위치기반 바인딩 사용하지말기
-//            Member result = em.createQuery("select m from Member m where m.username = ?1", Member.class)
-//                    .setParameter(1 "member1")
-//                    .getSingleResult();
-
-            System.out.println("singleResult = " + result.getUsername());
-
+            List<Member> result = em.createQuery("select m.team from Member m", Member.class)
+                    .getResultList();
+            Member findMember = result.get(0);
+            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e) {
