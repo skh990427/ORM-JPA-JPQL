1e5853ac3dde11c0e17bca9896cb04eaf5478576
KwangHo <skh990427@naver.com>
2024-02-22 23:55:32 +0900

    조인 - 연관관계 있는 외부 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -27,7 +27,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m, Team t where m.username = t.name";
+            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
